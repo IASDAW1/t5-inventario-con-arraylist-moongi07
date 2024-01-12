b947b12fa418b37e18771701668d60d41bc63cfa
Andrea <andreamartinezmoreno@hotmail.com>
2024-01-12 12:23:05 +0100

    prueba java

---
+++ hola/src/main/java/com/paclase/hola/Hola.java
@@ -0,0 +1,14 @@
+
+package com.paclase.hola;
+
+/**
+ *
+ * @author Andrea
+ */
+public class Hola {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hola");
+    }
+}
